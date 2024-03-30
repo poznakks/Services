8c3fdeed27759ad60c096857f15d64ba4762a578
Vlad <boguzhvlad@gmail.com>
2024-03-31 01:08:35 +0300

    first commit

---
+++ Services/ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesApp.swift
+//  Services
+//
+//  Created by Vlad Boguzh on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
